34db7ce68025dcc01131ddae897107084205bb4e
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-06-29 22:26:11 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -14,6 +14,10 @@ data "aws_ssm_parameter" "database_subnet_ids" {
     name = "/${var.project}/${var.environment}/database_subnet_ids"
 }
 
+data "aws_ssm_parameter" "mysql_sg_id" {
+    name = "/${var.project}/${var.environment}/mysql_sg_id"
+}
+
 data "aws_ami" "joindevops" {
   most_recent      = true
   owners           = ["973714476881"]
