f49a0c012d42344755436f780ed267447bb1d22f
akhilkumarmallineni-lgtm <akhilkumarmallineni@gmail.com>
2026-06-30 21:44:00 +0530

    terraform

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "Roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z1004002PSP33W9WXUA4"
+}  
+
+variable "domain_name" {
+    default = "akhilkumarshop.online"
+}
---
+++ 60-Catalogue/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "Roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "app_version" {
+    default = "v3"
+}
+
+/* variable "zone_id" {
+    default = ""
+}  
+
+variable "domain_name" {
+    default = ""
+} */
